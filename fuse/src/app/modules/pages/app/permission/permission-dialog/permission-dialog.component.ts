8d50c8a4a48fc30d05c2617a0acc035dd674663a
wahyudi.komite <adhye.yudhie@gmail.com>
2025-03-09 23:25:31 +0700

    Update from Workplus

--- fuse/src/app/modules/pages/app/permission/permission-dialog/permission-dialog.component.ts
+++ fuse/src/app/modules/pages/app/permission/permission-dialog/permission-dialog.component.ts
@@ -1,12 +1,108 @@
-import { Component } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    inject,
+    OnInit,
+} from '@angular/core';
+import {
+    FormBuilder,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {
+    MAT_DIALOG_DATA,
+    MatDialogActions,
+    MatDialogContent,
+    MatDialogRef,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Permission } from '../../../../../node/app/permission/permission';
+import { ExistingValidator } from '../../../../../node/common/existing.validator';
 
 @Component({
-  selector: 'app-permission-dialog',
-  standalone: true,
-  imports: [],
-  templateUrl: './permission-dialog.component.html',
-  styleUrl: './permission-dialog.component.scss'
+    selector: 'app-permission-dialog',
+    standalone: true,
+    imports: [
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatDialogContent,
+        MatDialogActions,
+        MatIconModule,
+    ],
+    templateUrl: './permission-dialog.component.html',
+    styleUrl: './permission-dialog.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PermissionDialogComponent {
+export class PermissionDialogComponent implements OnInit {
+    form!: FormGroup;
+    action?: string;
+    local_data: any;
+    permissions: Permission[] = [];
 
+    readonly dialogRef = inject(MatDialogRef<PermissionDialogComponent>);
+    readonly data = inject<Permission>(MAT_DIALOG_DATA);
+    private fb = inject(FormBuilder);
+    private existingValidator = inject(ExistingValidator);
+
+    ngOnInit(): void {
+        this.local_data = { ...this.data };
+        this.action = this.local_data.action;
+
+        this.form = this.fb.group({
+            name: [
+                '',
+                {
+                    validators: [Validators.required, Validators.minLength(4)],
+                    asyncValidators:
+                        this.action == 'Add'
+                            ? this.existingValidator.IsUnique(
+                                  'Permissions',
+                                  'Add'
+                              )
+                            : this.existingValidator.IsUnique(
+                                  'Permissions',
+                                  'Update'
+                              ),
+                },
+            ],
+            permissions: this.fb.array([]),
+        });
+
+        this.dialogRef.keydownEvents().subscribe((event) => {
+            if (event.key === 'Escape') {
+                this.closeDialog();
+            }
+        });
+
+        this.dialogRef.backdropClick().subscribe((event) => {
+            this.closeDialog();
+        });
+    }
+
+    doAction() {
+        console.log(1);
+
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.dialogRef.close({
+            event: this.action,
+            data: this.local_data,
+            formValue: this.form.value,
+        });
+    }
+
+    closeDialog() {
+        this.action = 'Cancel';
+        this.dialogRef.close({ event: 'Cancel' });
+    }
 }
